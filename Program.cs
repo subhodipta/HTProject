4a3f10907b6bd0510df52ff81baae8ce58640273
himadri <himadri@fi-tek.co.in>
2022-06-30 19:30:54 +0530

    commit

--- Program.cs
+++ Program.cs
@@ -14,7 +14,9 @@ namespace HTProject
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-        }
+        } 
+
+
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
